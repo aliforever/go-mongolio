66cdc90dc3bc9396d53e8a52a6956418ed7bebf6
aliforever <alipadida@live.com>
2022-03-06 18:07:00 +0330

    fixed a bug

--- collection.go
+++ collection.go
@@ -1,10 +1,13 @@
 package mongorm
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type col interface {
 	Name() string
-	ID() string
+	ID() primitive.ObjectID
 }
 
 type C[T col] struct {

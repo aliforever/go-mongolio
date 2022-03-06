30d10cb3ff84fa5aaa45ead450d5308f665b283e
aliforever <alipadida@live.com>
2022-03-06 21:33:16 +0330

    added delete method

---
+++ delete.go
@@ -0,0 +1,27 @@
+package mongorm
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func (c *C[T]) DeleteByID(id primitive.ObjectID, options ...*options.DeleteOptions) (result *mongo.DeleteResult, err error) {
+	var i T
+	result, err = c.db.Collection(i.CollectionName()).DeleteOne(context.Background(), bson.M{"_id": id}, options...)
+	return
+}
+
+func (c *C[T]) DeleteOne(filter bson.M, options ...*options.DeleteOptions) (result *mongo.DeleteResult, err error) {
+	var i T
+	result, err = c.db.Collection(i.CollectionName()).DeleteOne(context.Background(), filter, options...)
+	return
+}
+
+func (c *C[T]) DeleteMany(filter bson.M, options ...*options.DeleteOptions) (result *mongo.DeleteResult, err error) {
+	var i T
+	result, err = c.db.Collection(i.CollectionName()).DeleteMany(context.Background(), filter, options...)
+	return
+}

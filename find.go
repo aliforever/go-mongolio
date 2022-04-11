214f5c7827eaf5eb4a846fe2639b7152258dff9e
aliforever <alipadida@live.com>
2022-04-11 17:01:36 +0430

    added findbyid

--- find.go
+++ find.go
@@ -3,9 +3,16 @@ package mongorm
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func (c *C[T]) FindByID(id primitive.ObjectID, options ...*options.FindOneOptions) (result *T, err error) {
+	var i T
+	err = c.db.Collection(i.CollectionName()).FindOne(context.Background(), bson.M{"_id": id}, options...).Decode(&result)
+	return
+}
 
 func (c *C[T]) FindOne(m bson.M, options ...*options.FindOneOptions) (result *T, err error) {
 	var i T

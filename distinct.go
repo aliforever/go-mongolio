328e183be1f5d24d90ddd03250e612a4d2fd5ecd
aliforever <alipadida@live.com>
2022-04-12 13:06:38 +0430

    added distinct

---
+++ distinct.go
@@ -0,0 +1,19 @@
+package mongorm
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func (c *C[T]) Distinct(filter bson.M, fieldName string, options ...*options.DistinctOptions) (result []interface{}, err error) {
+	var (
+		i T
+	)
+	result, err = c.db.Collection(i.CollectionName()).Distinct(context.Background(), fieldName, filter, options...)
+	if err != nil {
+		return
+	}
+
+	return
+}
